f31af05fd78bd1982bd027fb8387e7a4324723b2
Stefan Eng <stefaneng13@gmail.com>
2016-02-06 15:41:30 -0800

    Simulate with a poisson distribution

--- trashcan.py
+++ trashcan.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python
 
+import numpy as np
+import time
+
+def simulate(cap, lamb, sleep):
+    """
+    Simulate a trash can with a poisson distribution with parameter lam.
+    Sleeps for `sleep` seconds between each count.
+    After `cap` capacity is reached, then trash can is full and stop.
+    """
+    count = 0
+    while count < cap:
+        # Generate one value with lambda as parameter
+        val = np.random.poisson(lamb)
+        print val
+        count += val
+        time.sleep(sleep)
+    return True
+
 if __name__ == '__main__':
-    print "Trashcan"
+    print simulate(30, 5, 1)
