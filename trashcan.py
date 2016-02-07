a219b1da9911d23fa02df47d0a8c9e1bb05ad205
Stefan Eng <stefaneng13@gmail.com>
2016-02-06 18:00:43 -0800

    Threads so trash cans perform concurrently

--- trashcan.py
+++ trashcan.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from threading import Thread
 import numpy as np
 import time
 import requests
@@ -15,10 +16,13 @@ def simulate(cap, lamb, sleep):
     while count < cap:
         # Generate one value with lambda as parameter
         val = np.random.poisson(lamb)
-        print val
         count += val
         time.sleep(sleep)
     return True
+
+def simAndAlert(trashname):
+    simulate(30, 5, 1)
+    print trashname, "full", alertApi(host, t, "full", "http").text
 
 def alertApi(host, trashname, endpoint, protocol):
     return requests.post("{}://{}/{}/{}".format(protocol, host, endpoint, trashname))
@@ -26,8 +30,9 @@ def alertApi(host, trashname, endpoint, protocol):
 if __name__ == '__main__':
     host = os.environ['TRASH_HOST']
     trashcans = ["trash" + str(i) for i in range(10)]
-    for t in trashcans:
-        print t, "inserted", alertApi(host, t, "empty", "http").text
-    for t in trashcans:
-        simulate(30, 5, 1)
-        print t, alertApi(host, t, "full", "http").text
+    for trash in trashcans:
+        print trash, "inserted", alertApi(host, trash, "empty", "http").text
+
+    for trash in trashcans:
+        t = Thread(target=simAndAlert, args=(trash,))
+        t.start()
